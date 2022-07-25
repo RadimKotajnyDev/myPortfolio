195bef389ff0396cfa5372c67215c3341c4e6ec5
kotajnyradim <kotajnyradim@sps-karvina.cz>
2022-07-25 15:03:41 +0200

    contact

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const Contact = () => {
+    return (
+        <div>
+            <Navbar />
+            <p>Here is my mail:</p>
+            <a href="radimkotajny@gmail.com">RadimKotajny@gmail.com</a>
+        </div>
+    );
+}
+
+export default Contact;
