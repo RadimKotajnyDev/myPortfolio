1a7c8a64da474b49cd660766f3effd4d9f220ad9
RadimKotajny <KotajnyRadim@sps-karvina.cz>
2022-07-31 17:51:02 +0200

    fixed props and optimized code

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import {motion} from "framer-motion";
 
-//TODO: shrink code with props
 //TODO: fix gray space sizing around nav buttons
 
+const aClasses = "bg-diffGray rounded-2xl p-2 hover:bg-opacity-50 duration-500 hover:text-white";
 
 const Navbar = () => {
+    const NavList = ({children}) => {
+        return (
+            <motion.li whileHover={{ scale:1.25 }} >
+                {children}
+            </motion.li>
+        );
+    }
     return (
         <div>
             <motion.div
@@ -16,36 +23,31 @@ const Navbar = () => {
                 <nav className="bg-lightGray drop-shadow-2xl
                     text-diffWhite font-medium rounded-3xl m-5 mx-[500px] mb-10">
                     <ul className="flex flex-row space-x-10 p-5 justify-center">
-                        <motion.li whileHover={{ scale:1.25 }}>
+                        <NavList>
                             <Link href="/">
-                                <a className="bg-diffGray rounded-2xl p-2 hover:bg-opacity-50
-                        duration-500 hover:text-white">Home</a>
+                                <a className={aClasses}>Home</a>
                             </Link>
-                        </motion.li>
-                        <motion.li whileHover={{ scale:1.25 }}>
+                        </NavList>
+                        <NavList>
                             <Link href="/about/me">
-                                <a className="bg-diffGray rounded-2xl p-2 hover:bg-opacity-50
-                        duration-500 hover:text-white">About me</a>
+                                <a className={aClasses}>About me</a>
                             </Link>
-                        </motion.li>
-                        <motion.li whileHover={{ scale:1.25 }}>
+                        </NavList>
+                        <NavList>
                             <Link href="/blog">
-                                <a className="bg-diffGray rounded-2xl p-2 hover:bg-opacity-50
-                        duration-500 hover:text-white">Blog posts</a>
+                                <a className={aClasses}>Blog posts</a>
                             </Link>
-                        </motion.li>
-                        <motion.li whileHover={{ scale:1.25 }}>
+                        </NavList>
+                        <NavList>
                             <Link href="/contact">
-                                <a className="bg-diffGray rounded-2xl p-2 hover:bg-opacity-50
-                        duration-500 hover:text-white">Contact</a>
+                                <a className={aClasses}>Contact</a>
                             </Link>
-                        </motion.li>
-                        <motion.li whileHover={{ scale:1.25 }}>
+                        </NavList>
+                        <NavList>
                             <Link href="/about/website">
-                                <a className="bg-diffGray rounded-2xl p-2 hover:bg-opacity-50
-                        duration-500 hover:text-white">About website</a>
+                                <a className={aClasses}>About website</a>
                             </Link>
-                        </motion.li>
+                        </NavList>
                     </ul>
                 </nav>
             </motion.div>
